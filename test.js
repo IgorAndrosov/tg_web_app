b0f6f10866d7fb6b1b3a5f9694757c2343117812
IgorAndrosov <igorandrosov2002@gmail.com>
2023-05-17 23:33:16 +0300

    update card

--- test.js
+++ test.js
@@ -1,42 +1,3 @@
-// Получаем кнопку "Добавить" и элемент счетчика
-/*const addButton = document.querySelector('.cafe-item-add-button');
-const counter = document.querySelector('.cafe-item-counter');
-const minusButton = document.querySelector('.cafe-item-decr-button');
-const plusButton = document.querySelector('.cafe-item-incr-button');
-
-// Обработчик события клика на кнопку "Добавить"
-addButton.addEventListener('click', function() {
-  // Увеличиваем значение счетчика на 1
-  counter.innerText = parseInt(counter.innerText) + 1;
-  
-  counter.style.display = 'inline-block';
-  addButton.style.display = 'none';
-
-  // Показываем кнопки "-" и "+"
-  minusButton.style.display = 'inline-block';
-  plusButton.style.display = 'inline-block';
-});
-
-minusButton.addEventListener('click', function() {
-
-    if (counter.innerText == 1) {
-        
-        minusButton.style.display = 'none';
-        plusButton.style.display = 'none';
-        counter.style.display = 'none';
-        addButton.style.display = 'inline-block';
-    };
-
-    counter.innerText = parseInt(counter.innerText) - 1;
-
-});
-
-plusButton.addEventListener('click', function() {
-
-    counter.innerText = parseInt(counter.innerText) + 1;
-});*/
-
-
 // Данные для элементов кафе-меню
 const menuData = [
     {
@@ -143,7 +104,8 @@ addButton.forEach((button, index) => {
     const currentValue = parseInt(counter[index].textContent);
     counter[index].textContent = currentValue + 1;
     updateCart(); // Обновляем корзину при изменении значения счетчика
-
+    updateCartButton();
+    cartButton.style.display = 'inline-block';
     button.style.display = 'none';
     counter[index].style.display = 'inline-block';
 
@@ -168,6 +130,8 @@ minusButton.forEach((button, index) => {
 
       counter[index].textContent = currentValue - 1;
       updateCart(); // Обновляем корзину при изменении значения счетчика
+      updateCartButton();
+
     });
   });
   
@@ -176,6 +140,7 @@ minusButton.forEach((button, index) => {
         const currentValue = parseInt(counter[index].textContent);
         counter[index].textContent = parseInt(counter[index].textContent) + 1;
         updateCart(); // Обновляем корзину при изменении значения счетчика
+        updateCartButton();
     });
   });
 
@@ -203,3 +168,70 @@ minusButton.forEach((button, index) => {
   totalElement.textContent = `Общая сумма заказа: $${total.toFixed(2)}`;
   cart.appendChild(totalElement);
 };
+
+const cartButton = document.getElementById("cart-button");
+
+// Обработчик события для кнопки "Корзина"
+cartButton.addEventListener("click", () => {
+  showCartDetails();
+});
+
+function updateCartButton() {
+    let total = calculateTotal();
+  
+    // Обновляем текст кнопки с суммой заказа
+    cartButton.textContent = `Корзина ($${total.toFixed(2)})`;
+  };
+
+  function calculateTotal() {
+    let total = 0;
+  
+    // Рассчитываем общую сумму заказа
+    for (let i = 0; i < menuData.length; i++) {
+      const currentValue = parseInt(counter[i].textContent);
+      const itemPrice = parseFloat(menuData[i].price.replace("$", ""));
+      const itemTotal = itemPrice * currentValue;
+  
+      total += itemTotal;
+    }
+  
+    return total;
+  }
+  
+  // Обновляем кнопку при изменении количества товаров в корзине
+  function updateButtonOnQuantityChange() {
+    const addButton = document.querySelectorAll('.add-button');
+    const minusButton = document.querySelectorAll('.minus-button');
+    const plusButton = document.querySelectorAll('.plus-button');
+  
+    addButton.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        // Обработчик события при нажатии кнопки "Добавить"
+  
+        // Ваш код для изменения количества и других действий
+  
+        updateCartButton(); // Обновляем текст кнопки с суммой заказа
+      });
+    });
+};
+
+function showCartDetails() {
+  let cartItems = ""; // Строка для хранения деталей заказа
+  let total = 0; // Переменная для отслеживания общей суммы заказа
+
+  // Формируем детали заказа
+  for (let i = 0; i < menuData.length; i++) {
+    const currentValue = parseInt(counter[i].textContent);
+    const itemPrice = parseFloat(menuData[i].price.replace("$", ""));
+    const itemTotal = itemPrice * currentValue;
+
+    // Добавляем детали заказа, если количество больше нуля
+    if (currentValue > 0) {
+      cartItems += `${menuData[i].name}: ${currentValue} x $${itemPrice.toFixed(2)} = $${itemTotal.toFixed(2)}\n`;
+      total += itemTotal;
+    }
+  }
+
+  // Отображаем всплывающее окно с деталями заказа
+  alert(`Детали заказа:\n\n${cartItems}\nОбщая сумма заказа: $${total.toFixed(2)}`);
+}
